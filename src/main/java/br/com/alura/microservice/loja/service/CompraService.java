9cd1fea3e5f40793574d52e1738bd18814d8c53e
marcb <marcbitenl@gmail.com>
2022-07-08 20:46:17 -0300

    Implementacao com Spring Feign

--- src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -1,39 +1,23 @@
 package br.com.alura.microservice.loja.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
+import br.com.alura.microservice.loja.client.FornecedorClient;
 import br.com.alura.microservice.loja.controller.dto.CompraDTO;
 import br.com.alura.microservice.loja.controller.dto.InfoFornecedorDTO;
 
 @Service
 public class CompraService {
-	
+
 	@Autowired
-	private RestTemplate client;
-	
-	@Autowired
-	private DiscoveryClient eurekaClient;
-	
+	private FornecedorClient fornecedorClient;
+
 	public void realizaCompra(CompraDTO compra) {
 		
+		InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(compra.getEndereco().getEstado());
 
-
-		
-		ResponseEntity<InfoFornecedorDTO> exchange =
-				client.exchange("http://fornecedor/info/GO" + compra.getEndereco().getEstado(),
-				HttpMethod.GET, null, InfoFornecedorDTO.class);
-		
-		eurekaClient.getInstances("fornecedor").stream()
-		.forEach(fornecedor -> {
-			System.out.println("localhost:" + fornecedor.getPort());
-		});
-		
-		System.out.println(exchange.getBody().getEndereco());
+		System.out.println(info.getEndereco());
 	}
 
 }
